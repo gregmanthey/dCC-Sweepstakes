bcb17cd7ebc241033e20a718f68f79936bf21b45
gregmanthey <gmanthey03@gmail.com>
2019-09-18 09:28:42 -0500

    Added methods to Sweepstakes class as outlined in the user story.

--- SweepstakesProject/Sweepstakes.cs
+++ SweepstakesProject/Sweepstakes.cs
@@ -8,5 +8,23 @@ namespace SweepstakesProject
 {
   class Sweepstakes
   {
+    public Sweepstakes(string name)
+    {
+
+    }
+
+    void RegisterContestant(Contestant contestant)
+    {
+
+    }
+    Contestant PickWinner()
+    {
+      Contestant winner = new Contestant();
+      return winner;
+    }
+    void PrintContestantInfo(Contestant contestant)
+    {
+
+    }
   }
 }
