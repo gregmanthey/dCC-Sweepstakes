f0682a0de625be688d0a5b31946357fd32084983
gregmanthey <gmanthey03@gmail.com>
2019-09-18 09:54:20 -0500

    Added inheritance of ISweepstakesManager to SweepstakesStackManager and skeleton methods.

--- SweepstakesProject/SweepstakesStackManager.cs
+++ SweepstakesProject/SweepstakesStackManager.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace SweepstakesProject
 {
-  class SweepstakesStackManager
+  class SweepstakesStackManager : ISweepstakesManager
   {
+    public Sweepstakes GetSweepstakes()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void InsertSweepstakes(Sweepstakes sweepstakes)
+    {
+      throw new NotImplementedException();
+    }
   }
 }
