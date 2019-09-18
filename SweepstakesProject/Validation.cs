189c98b7cd490ed92cb29bd4962ad72c9c1ff60e
gregmanthey <gmanthey03@gmail.com>
2019-09-18 14:33:55 -0500

    Added Validation class, copied methods AlphanumericCharactersAndSpace, AlphabetCharacters, NumericCharacters, NumericCharactersWithDecimal, YesNo from LemonadeStand project. Will trim excess methods later on.

---
+++ SweepstakesProject/Validation.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SweepstakesProject
+{
+  class Validation
+  {
+    public static bool AlphanumericCharactersAndSpace(string input)
+    {
+      Regex nonAlphaNumericChars = new Regex("[^a-zA-Z0-9 ]");
+      if (!nonAlphaNumericChars.IsMatch(input))
+      {
+        return true;
+      }
+      Console.WriteLine("Please enter only numbers, letters and spaces.");
+      return false;
+    }
+    public static bool AlphabetCharacters(string input)
+    {
+      Regex nonAlphabetChars = new Regex("[^a-zA-Z]");
+      if (!nonAlphabetChars.IsMatch(input))
+      {
+        return true;
+      }
+      Console.WriteLine("Please enter only letters.");
+      return false;
+    }
+    public static bool NumericCharacters(string input)
+    {
+      Regex nonNumericChars = new Regex("[^0-9]");
+      if (!nonNumericChars.IsMatch(input))
+      {
+        return true;
+      }
+      Console.WriteLine("Invalid input. Please enter only a number.");
+      return false;
+    }
+    public static bool NumericCharactersWithDecimal(string input)
+    {
+      Regex nonNumericOrDecimalChars = new Regex("[^0-9.]");
+      if (!nonNumericOrDecimalChars.IsMatch(input) && input.Contains("."))
+      {
+        return true;
+      }
+      Console.WriteLine("Invalid input. Please enter a number with a decimal.");
+      return false;
+    }
+   
+    public static bool YesNo(string input)
+    {
+      if (input.ToLower() == "yes" ||
+          input.ToLower() == "y" ||
+          input.ToLower() == "no" ||
+          input.ToLower() == "n")
+      {
+        return true;
+      }
+      Console.WriteLine("Please enter yes (y) or no (n).");
+      return false;
+    }
+  }
+}
