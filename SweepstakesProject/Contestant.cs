06097313e030dd642f4915cad8cec6f581b7d042
gregmanthey <gmanthey03@gmail.com>
2019-09-18 09:22:34 -0500

    Added base project, set up base structure as outlined in the user stories (classes Contestant, UI, Sweepstakes, SweepstakesStackManager, SweepstakesQueueManager, MarketingFirm, interface ISweepstakesManager). Created member variables for Contestant class.

---
+++ SweepstakesProject/Contestant.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProject
+{
+  class Contestant
+  {
+    string firstName;
+    string lastName;
+    string emailAddress;
+    int registrationNumber;
+  }
+}
