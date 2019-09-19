26c0a437bddc0baa2ed9e10646f835cf4f3726dc
gregmanthey <gmanthey03@gmail.com>
2019-09-19 09:16:54 -0500

    Added AddContestant method to MarketingFirm.

--- SweepstakesProject/MarketingFirm.cs
+++ SweepstakesProject/MarketingFirm.cs
@@ -9,7 +9,16 @@ namespace SweepstakesProject
   class MarketingFirm
   {
     ISweepstakesManager sweepstakesManager;
+    Sweepstakes currentSweepstakes;
 
+    public void AddContestant()
+    {
+      string firstName = UI.PromptFor("What is the contestant's first name?", Validation.AlphabetCharacters);
+      string lastName = UI.PromptFor("What is the contestant's last name?", Validation.AlphabetCharacters);
+      string emailAddress = UI.PromptFor("What is the contestant's email address?", Validation.EmailCharacters);
+      Contestant contestant = new Contestant(firstName, lastName, emailAddress);
+      currentSweepstakes.RegisterContestant(contestant);
+    }
     public void BeginSweepstakes()
     {
       Sweepstakes newSweepstakes = new Sweepstakes(UI.PromptFor("What do you want to name the sweepstakes?", Validation.AlphanumericCharactersAndSpace));
