cb4d60700c0f676f0e53a65fb66eee8e12ec397b
gregmanthey <gmanthey03@gmail.com>
2019-09-19 08:12:30 -0500

    Added methods Display and Pause to UI class.

--- SweepstakesProject/UI.cs
+++ SweepstakesProject/UI.cs
@@ -18,5 +18,14 @@ namespace SweepstakesProject
       } while (userInput == null || userInput == "" || !valid(userInput));
       return userInput;
     }
+    public static void Display(string content)
+    {
+      Console.WriteLine(content);
+    }
+    public static void Pause()
+    {
+      Console.ReadLine();
+      Console.WriteLine("Press Enter to continue.");
+    }
   }
 }
