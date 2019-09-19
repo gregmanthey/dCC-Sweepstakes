040c93f79bc4621ac5f63f464a5f579e9b07b13e
gregmanthey <gmanthey03@gmail.com>
2019-09-19 11:04:10 -0500

    Added SweepstakesManagerFactory class to pick between stack and queue and instantiate and return accordingly.

---
+++ SweepstakesProject/SweepstakesManagerFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweepstakesProject
+{
+  static class SweepstakesManagerFactory
+  {
+    public static ISweepstakesManager ChooseSweepstakesManager(string choice)
+    {
+      ISweepstakesManager sweepstakesManager = null;
+      switch (choice)
+      {
+        case "queue":
+          sweepstakesManager = new SweepstakesQueueManager();
+          break;
+        case "stack":
+          sweepstakesManager = new SweepstakesStackManager();
+          break;
+        default:
+          break;
+      }
+      return sweepstakesManager;
+    }
+  }
+}
